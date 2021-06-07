9a2ebfe4c7eb948ebe622e2ed793ce64980d42b6
luojbin <z1_2x@msn.com>
2021-06-07 18:07:21 +0800

    customize gatefilter

--- src/main/java/com/luojbin/demo/gateway/filter/route/factory/MyArgsGatewayFilterFactory.java
+++ src/main/java/com/luojbin/demo/gateway/filter/route/factory/MyArgsGatewayFilterFactory.java
@@ -34,6 +34,30 @@ public class MyArgsGatewayFilterFactory extends AbstractGatewayFilterFactory<MyA
         private String name;
         private String age;
         private String address;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
     }
     // endregion 自定义配置类
 
